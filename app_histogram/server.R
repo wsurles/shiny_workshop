1d85ba482f407d142f9e86501bba7bdda346db97
William Surles <williamsurles@gmail.com>
2014-03-16 16:26:22 -0600

    removed code from server and ui in histogram app

--- app_histogram/server.R
+++ app_histogram/server.R
@@ -1,24 +1,3 @@
 ##| server.R
 
-shinyServer(function(input,output){
-
-  output$main_plot <- renderPlot({  
-    hist(faithful$eruptions,
-           breaks = as.numeric(input$n_breaks),
-           col = "green",
-           xlab = "Duration (minutes)")  
-  })
-  
-  output$second_plot <- renderPlot({
-    hist(faithful$waiting,
-         breaks = as.numeric(input$n_breaks),
-         col = "darkorange2",
-         xlab = "Wait (minutes)")
-  })
-  
-  output$third_plot <- renderPlot({
-    plot(faithful$waiting,faithful$eruptions,
-         col = "darkblue"
-         )
-  })
-})
+shinyServer(function(input,output){})
--- app_histogram/ui.R
+++ app_histogram/ui.R
@@ -2,19 +2,6 @@
 
 shinyUI(pageWithSidebar(
   headerPanel("Simple Histogram App"),
-  sidebarPanel(
-    selectInput(inputId = "n_breaks",
-                label = "Number of bins",
-                choices = c(10, 20, 25, 30, 50),
-                selected = 20),
-    h5("Choose plots to show:"),
-    checkboxInput("main_plot", "duration", value = TRUE),
-    checkboxInput("second_plot", "wait", value = FALSE),
-    checkboxInput("third_plot", "duration vs wait", value = FALSE)
-    ),
-  mainPanel(
-    conditionalPanel(condition = "input.main_plot == true", plotOutput(outputId = "main_plot")),
-    conditionalPanel(condition = "input.second_plot == true", plotOutput(outputId = "second_plot")),
-    conditionalPanel(condition = "input.third_plot == true", plotOutput(outputId = "third_plot"))
-    )
+  sidebarPanel(),
+  mainPanel()
   ))
