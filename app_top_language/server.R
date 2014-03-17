e2526b6226342431288431c63c86048b669bdbd0
William Surles <williamsurles@gmail.com>
2014-03-16 20:04:29 -0600

    added reactive lang

--- app_top_language/server.R
+++ app_top_language/server.R
@@ -9,24 +9,30 @@ shinyServer(function(input, output) {
   ## load data
   data <- read.csv("data/event_lang_day.csv", stringsAsFactor = F)
   
-  ## summarize events by language
-  lang <- ddply(data, .(type, repository_language), summarise,
-                num_event = sum(count_event))
-  lang <- subset(lang,type == "PushEvent")
-  lang <- lang[order(lang$num_event, decreasing=T),]
+  getTopLang <- reactive({
+    ##| Find top languages by eventtype 
   
-  ## find top languages and put languages in order 
-  top_lang <- lang[1:20,]
-  top_lang <- transform(top_lang, repository_language=reorder(repository_language, num_event)) 
-  
-    output$main_plot <- renderPlot({    
-      ## make a bar chart of the top languages
-      p <- ggplot(top_lang, aes(repository_language, num_event)) +
-        geom_bar(stat="identity", fill = 'steelblue', alpha = .7) +
-        coord_flip()
-      print(p)
+    ## summarize events by language
+    lang <- ddply(data, .(type, repository_language), summarise,
+                  num_event = sum(count_event))
+    lang <- subset(lang,type == input$event_type)
+    lang <- lang[order(lang$num_event, decreasing=T),]
     
-    })
+    ## find top languages and put languages in order 
+    top_lang <- lang[1:20,]
+    top_lang <- transform(top_lang, repository_language=reorder(repository_language, num_event)) 
+  })
+  
+  output$main_plot <- renderPlot({        
+    ## make a bar chart of the top languages
+
+    top_lang <- getTopLang()
+    p <- ggplot(top_lang, aes(repository_language, num_event)) +
+      geom_bar(stat="identity", fill = 'steelblue', alpha = .7) +
+      coord_flip()
+    print(p)
+  
+  })
 })
 
 
