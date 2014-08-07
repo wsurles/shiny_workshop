8edcb30d7947c4b47960d797cf62345dc974ca83
William Surles <williamsurles@gmail.com>
2014-08-07 12:15:26 -0600

    moved github hot or not app to separate repo

--- app_github_hot/cron/cron.sh
+++
@@ -1,3 +0,0 @@
-#/bin/sh
-cd /srv/shiny-server/default/apps/app_github_hot
-/usr/bin/R --vanilla --file=/srv/shiny-server/default/apps/app_github_hot/cron/get_data.R
--- app_github_hot/help/style.R
+++
@@ -1,5 +0,0 @@
-helpfiles <- list.files(".", pattern = "*.md")
-for(hf in helpfiles) {
-	knit2html(hf, options = "", stylesheet = "empty.css")
-	markdownToHTML(inputfile, outputfile, options = c(""), stylesheet='empty.css')  
-}
